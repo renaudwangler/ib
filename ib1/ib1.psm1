bcaa564a9f305f71e849cd807fc4b9cc9068d108
renaudwangler <renaud.wangler@ib.cegos.fr>
2018-01-19 14:33:12 +0100

    1.2.15

--- ib1/ib1.psm1
+++ ib1/ib1.psm1
@@ -676,6 +676,8 @@ if (-not (Get-ScheduledTask -TaskName 'Lancement ibInit' -ErrorAction 0)) {
   $trigger=New-ScheduledTaskTrigger -AtStartup
   Register-ScheduledTask -Action $CMDTask,$PSTask -AsJob -TaskName 'Lancement ibInit' -Description "Lancement de l'initialisation ib" -Trigger $trigger -user 'NT AUTHORITY\SYSTEM' -RunLevel Highest}
 write-debug 'Création du fichier c:\windows\ibInit.cmd'
+$moduleVersion=(get-Module -ListAvailable -Name ib1|sort-object|select-object -last 1).version.tostring()
+copy $env:ProgramFiles\windowspowershell\Modules\ib1\1.2.15\$moduleVersion\ibInit.cmd c:\windows\ibInit.cmd
 'powershell.exe -command "& set-executionpolicy bypass -force; $secondsToWait=5; While (($secondsToWait -gt 0) -and (-not(test-NetConnection))) {$secondsToWait--;start-sleep 1}; if (get-module -ListAvailable -name ib1) {update-module ib1 -force} else {install-module ib1 -force}"'|out-file -filePath $env:SystemRoot\ibInit.cmd -encoding unicode
 write-debug 'Création des raccourcis'
 new-ib1Shortcut -File '%windir%\System32\mmc.exe' -Params '%windir%\System32\virtmgmt.msc' -title 'Hyper-V Manager' -icon '%ProgramFiles%\Hyper-V\SnapInAbout.dll,0'

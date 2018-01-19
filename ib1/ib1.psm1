0d0b9284cfc51ad9370c59425c0113ae124489c8
renaudwangler <renaud.wangler@ib.cegos.fr>
2018-01-19 10:19:13 +0100

    1.1.57

--- ib1/ib1.psm1
+++ ib1/ib1.psm1
@@ -639,7 +639,7 @@ write-debug 'Création du fichier c:\windows\ibInit.cmd'
 echo '@echo off' > $env:SystemRoot\ibInit.cmd
 echo 'powershell.exe -command "& set-executionpolicy bypass -force; $secondsToWait=5; While (($secondsToWait -gt 0) -and (-not(test-NetConnection))) {$secondsToWait--;start-sleep 1}; if (get-module -ListAvailable -name ib1) {update-module ib1 -force} else {install-module ib1 -force}"' >> $env:SystemRoot\ibInit.cmd
 write-debug 'Création des raccourcis'
-new-ib1Shortcut -File '%windir%\System32\mm.exe "%windir%\System32\Wrtmgmt.msc" -title 'Hyper-V Manager'
+new-ib1Shortcut -File '%windir%\System32\mm.exe "%windir%\System32\virtmgmt.msc"' -title 'Hyper-V Manager'
 new-ib1Shortcut -File '%SystemRoot%\System32\WindowsPowershell\v1.0\powershell.exe' -title 'Windows PowerShell'
 new-ib1Shortcut -URL 'https://eval.ib-formation.com/avis' -title 'Mi-Parcours'
 new-ib1Shortcut -URL 'https://eval.ib-formation.com'

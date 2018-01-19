a376a4d8144829bfa72778f0bb0b4102aae70e9e
renaudwangler <renaud.wangler@ib.cegos.fr>
2018-01-19 14:53:06 +0100

    1.2.23
    Lancement des scripts planifiés depuis dossier du module

--- ib1/ib1.psm1
+++ ib1/ib1.psm1
@@ -669,13 +669,13 @@ else {
   write-debug 'Installation de Hyper-V'
   enable-WindowsOptionalFeature -Online -FeatureName:Microsoft-Hyper-V-All
   write-warning "Relancer la commande après redémarrage pour finaliser la confirguration d'Hyper-V"}
-if (-not (Get-ScheduledTask -TaskName 'Lancement ibInit' -ErrorAction 0)) {
-  write-Debug 'Création de la tâche de lancement de ibInit'
-  $moduleVersion=(get-Module -ListAvailable -Name ib1|sort-object|select-object -last 1).version.tostring()
-  $CMDTask=New-ScheduledTaskAction -Execute "$env:ProgramFiles\windowspowershell\Modules\ib1\1.2.15\$moduleVersion\ibInit.cmd"
-  $PSTask= New-ScheduledTaskAction -Execute 'powershell.exe' -argument '-noprofile -windowStyle Hidden -command "& $env:ProgramFiles\windowspowershell\Modules\ib1\1.2.15\$moduleVersion\ibInit.ps1"'
-  $trigger=New-ScheduledTaskTrigger -AtStartup
-  Register-ScheduledTask -Action $CMDTask,$PSTask -AsJob -TaskName 'Lancement ibInit' -Description "Lancement de l'initialisation ib" -Trigger $trigger -user 'NT AUTHORITY\SYSTEM' -RunLevel Highest}
+write-Debug 'Création de la tâche de lancement de ibInit'
+if (Get-ScheduledTask -TaskName 'Lancement ibInit' -ErrorAction 0) {Get-ScheduledTask -TaskName 'Lancement ibInit'|unregister-scheduledTask}
+$moduleVersion=(get-Module -ListAvailable -Name ib1|sort-object|select-object -last 1).version.tostring()
+$CMDTask=New-ScheduledTaskAction -Execute "$env:ProgramFiles\windowspowershell\Modules\ib1\1.2.15\$moduleVersion\ibInit.cmd"
+$PSTask= New-ScheduledTaskAction -Execute 'powershell.exe' -argument '-noprofile -windowStyle Hidden -command "& $env:ProgramFiles\windowspowershell\Modules\ib1\1.2.15\$moduleVersion\ibInit.ps1"'
+$trigger=New-ScheduledTaskTrigger -AtStartup
+Register-ScheduledTask -Action $CMDTask,$PSTask -AsJob -TaskName 'Lancement ibInit' -Description "Lancement de l'initialisation ib" -Trigger $trigger -user 'NT AUTHORITY\SYSTEM' -RunLevel Highest
 write-debug 'Création du fichier c:\windows\ibInit.cmd'
 write-debug 'Création des raccourcis'
 new-ib1Shortcut -File '%windir%\System32\mmc.exe' -Params '%windir%\System32\virtmgmt.msc' -title 'Hyper-V Manager' -icon '%ProgramFiles%\Hyper-V\SnapInAbout.dll,0'

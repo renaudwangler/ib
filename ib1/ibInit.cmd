f5073988b0787967f17e19d26d67d3444f8fe8ff
renaudwangler <renaud.wangler@ib.cegos.fr>
2018-01-19 14:37:28 +0100

    1.2.16

--- ib1/ibInit.cmd
+++ ib1/ibInit.cmd
@@ -1,2 +1,2 @@
-@echo off
-powershell.exe -command "& set-executionpolicy bypass -force; $secondsToWait=5; while (($secondsToWait -gt 0) -and (-not (test-Netconnection))) {$secondsToWait--; start-sleep 1}; if (get-module -ListAvailable -name ib1) {update-module ib1 -force} else {install-module ib1 -force}"
+﻿@echo off
+powershell.exe -command "& set-executionpolicy bypass -force; $secondsToWait=5; While (($secondsToWait -gt 0) -and (-not(test-NetConnection))) {$secondsToWait--;start-sleep 1}; if (get-module -ListAvailable -name ib1) {update-module ib1 -force} else {install-module ib1 -force}"

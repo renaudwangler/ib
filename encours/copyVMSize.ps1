1a2badf979d0d251e93f8227e171c68e26642d4e
renaudwangler <renaud.wangler@ib.cegos.fr>
2018-07-03 10:38:52 -0700

    copyVMSize. v1

---
+++ encours/copyVMSize.ps1
@@ -0,0 +1,17 @@
+﻿if (!(Get-Variable locationSizes -ErrorAction SilentlyContinue)) {
+  Connect-AzureRmAccount
+  $locationSizes=@{}
+  foreach ($location in (Get-AzureRmLocation)) {
+    Write-Progress -Activity "Retreiving available VM sizes by location for the '$((Get-AzureRmSubscription).Name)' subscription." -CurrentOperation $location.DisplayName
+    if ($location.Providers -icontains 'Microsoft.compute') {
+      $cpuLimit=(Get-AzureRmVMUsage -Location $location.Location|where {$_.name.value -like 'cores'}).Limit
+      $vmSizes=(Get-AzureRmVMSize -Location $location.Location|where NumberOfCores -le $cpuLimit).Name
+      $localSizes=(Get-AzureRmComputeResourceSku |where {$_.ResourceType -like 'virtualmachines' -and $_.Restrictions.reasoncode -notlike 'notavailableforsubscription' -and $_.LocationInfo.location -eq $location.Location -and $vmSizes -contains $_.Name}).Name
+      if ($localSizes.Count -ne 0) {
+        $locationSizes.$($location.DisplayName)=$localSizes
+      }
+    }
+  }
+  Write-Progress -Activity "Retreiving available VM sizes by location for the '$((Get-AzureRmSubscription).Name)' subscription." -Completed
+}
+Set-Clipboard (($locationSizes|Out-GridView -Title 'Vm Size by location' -PassThru).value|Out-GridView -title 'Choose VM size for clipboard' -PassThru)

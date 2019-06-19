423ebb65ff4730732d06e79bba8f28bdcfbc86f7
renaudwangler <renaud.wangler@ib.cegos.fr>
2019-06-19 17:59:25 +0200

    2.5.7w1.0
    Version initiale (post debug) des "APTS"

--- docs/apts.js
+++ docs/apts.js
@@ -1,12 +1,12 @@
 ﻿function aptsLoad() {
-  document.body.innerHTML='<h1>'+ibCourse+' - Accompagnement Pédagogique et Technique de stage</h1><div class="edition">Dernière édition le '+ibEdition+'</div>'+document.body.innerHTML
+  modDate=new Date(document.lastModified);
+  document.body.innerHTML='<h1>'+ibCourse+' - Accompagnement Pédagogique et Technique de stage</h1><div class="edition">(Dernière édition le '+modDate.getDate()+'/'+modDate.getMonth()+'/'+modDate.getFullYear()+')</div>'+document.body.innerHTML+'<div id="master1"></div><div id="master0"></div><div id="outro"></div>';
   document.getElementById('custo').innerHTML='<h2>Customisation spécifique du stage '+ibCourse+'</h2>'+document.getElementById('custo').innerHTML+'<hr/>';
-  document.getElementById('conseils').innerHTML='<h2>Conseils d\'animation pour le stage '+ibCourse+'</h2>'+document.getElementById('conseils').innerHTML+'<br/>Prendre connaissance du fichier readme posé sur le bureau et suggérer aux stagiaires de faire de même.<br/>En cas de soucis sur un poste, il est possible de (re)lancer la commande <code>ibSetup '+ibCourse+' -force</code> pour mettre en place ou corriger les éléments de customisation décrits précédemment.<br/>(y ajouter l\'option "<code>-trainer</code>" sur le poste du formateur).<br/>';
-  readFile('2019old/master1.html','master1');
-  readFile('2019old/master0.html','master0');
+  document.getElementById('conseils').innerHTML='<h2>Conseils d\'animation pour le stage '+ibCourse+'</h2>'+document.getElementById('conseils').innerHTML+'<div id="conseilsGeneriques">Prendre connaissance du fichier readme posé sur le bureau et suggérer aux stagiaires de faire de même.<br/>En cas de soucis sur un poste, il est possible de (re)lancer la commande <code>ibSetup '+ibCourse+' -force</code> pour mettre en place ou corriger les éléments de customisation décrits précédemment.<br/>(y ajouter l\'option "<code>-trainer</code>" sur le poste du formateur).</div>';
+  readFile(ibMaster1+'/master1.html','master1');
+  readFile(ibMaster0+'/master0.html','master0');
   readFile('outro.html','outro');
-  document.title=ibCourse+' - APTS';
-  document.getElementsByTagName('h1')[0].innerHTML=ibCourse+' - Accompagnement Pédagogique et Technique de stage';}
+  document.title=ibCourse+' - APTS';}
 
 function readFile(fileName,divID) {
   var txtFile=new XMLHttpRequest();

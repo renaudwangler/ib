4bcfbc36b91aceaf3e39deef2e7ab930fbfd0b87
renaudwangler <renaud.wangler@ib.cegos.fr>
2022-02-08 18:09:44 +0100

    display

--- docs/apts.js
+++ docs/apts.js
@@ -52,9 +52,9 @@ function aptsLoad() {
   var pageName = window.location.pathname.split('/').pop().split('.')[0];
   document.body.innerHTML='<h1>'+pageName+' - Accompagnement Pédagogique et Technique de stage<a href="index.html" title="Retour à la liste." id="back"><img src="logo_ib.png" alt="Retour"></a></h1>'+document.body.innerHTML+'<div id="outro"></div>';
   if (document.getElementById('conseils')) {document.getElementById('conseils').innerHTML='<h2>Conseils d\'animation pour le stage '+pageName+'</h2>'+document.getElementById('conseils').innerHTML;}
-  if (document.getElementById('goDeploy')) {readFile('goDeploy.html','goDeploy',false);}
-  if (document.getElementById('o365')) {readFile('o365.html','o365',false);}
-  if (document.getElementById('Azure')) {readFile('Azure.html','Azure',false);}
+  if (document.getElementById('goDeploy')) {readFile('goDeploy.html','goDeploy');}
+  if (document.getElementById('o365')) {readFile('o365.html','o365');}
+  if (document.getElementById('Azure')) {readFile('Azure.html','Azure');}
   readFile('outro.html','outro');
   document.title=pageName+' - APTS';
 
@@ -69,13 +69,12 @@ function aptsLoad() {
   document.getElementsByTagName('head')[0].appendChild(iconRef);
 }
 
-function readFile(fileName,divID,noHr=false) {
+function readFile(fileName,divID) {
   var txtFile=new XMLHttpRequest();
   txtFile.open('GET',fileName,true);
   txtFile.onreadystatechange = function() {
     if (txtFile.readyState === 4) {
       if (txtFile.status === 200) {
         var customTextElement = document.getElementById(divID);
-        if (noHr) {customTextElement.innerHTML = txtFile.responseText;} 
-        else {customTextElement.innerHTML = '<hr/>'+txtFile.responseText;}}}}
+        customTextElement.innerHTML = '<hr/>'+txtFile.responseText;}}}
 txtFile.send(null);}
